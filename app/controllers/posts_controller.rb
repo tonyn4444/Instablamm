5844a75cc4d53fdc2e68c61c70f9b09e95788242
Tony Nguyen <tntuyen@ucdavis.edu>
2016-12-29 12:29:36 -0800

    adds a user interface command bar at top of every page, begins development of user posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,18 @@ class PostsController < ApplicationController
 	before_filter :authorize
 	
 	def index
+		
 	end
 
 	def new
 	end
 
 	def create
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:caption, :user_id)
 	end
 end
